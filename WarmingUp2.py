3eb9f4f441af8578273a46d6b6db32aad37f8453
ddonnie <mohax91@gmail.com>
2016-03-09 20:17:11 +0200

    added WarmingUp2.py

---
+++ WarmingUp2.py
@@ -0,0 +1,48 @@
+#Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+def string_times(str, n):
+    return str*n
+
+#Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars
+#or whatever is there if the string is less than length 3. Return n copies of the front;
+def front_times(str, n):
+    return str[:3]*n
+
+#Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+def string_bits(str):
+    return str[::2]
+
+#Given a non-empty string like "Code" return a string like "CCoCodCode".
+def string_splosion(str):
+    res = ''
+    for i in range(len(str)): res += str[:i]
+    return res+str
+
+#Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the
+#last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+def last2(str):
+    count = 0
+    if len(str)>2:
+        for i in range(len(str)-2):
+            if str[i:i+2]==str[-2:]: count +=1
+    return count
+
+#Given an array of ints, return the number of 9's in the array.
+def array_count9(nums):
+    return nums.count(9)
+
+#Given an array of ints, return True if one of the first 4 elements in the array is a 9.
+#The array length may be less than 4.
+
+def array123(nums):
+    return str(nums).find('1, 2, 3')>=0
+
+#Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+#So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+def string_match(a, b):
+    count = 0
+    l = len(a)
+    if len(a)>len(b): l = len(b)
+    for i in range(l-1):
+        if a[i:i+2]==b[i:i+2]: count+=1
+    return count
