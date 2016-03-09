33df669bc7b1a855db37ca89c3d95803c4bc49b1
ddonnie <mohax91@gmail.com>
2016-03-09 20:22:47 +0200

    added WarmingUp2.py

--- WarmingUp.py
+++ WarmingUp.py
@@ -64,3 +64,4 @@ def front_back(str):
 #If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
 def front3(str):
     return str[:3]*3
+
